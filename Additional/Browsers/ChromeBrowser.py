9381abb999bb93d8a331708e90cf362a263552c0
vimaniek <mariusznn@gmail.com>
2018-06-24 22:11:44 +0200

    Small change in path to chromedriver

--- Additional/Browsers/ChromeBrowser.py
+++ Additional/Browsers/ChromeBrowser.py
@@ -3,7 +3,7 @@ from selenium import webdriver
 
 class ChromeBrowser:
 
-    driver = webdriver.Chrome(r'C:\Users\Mariusz\IdeaProjects\PythonTestExercises2\Additional\Drivers\chromedriver.exe')
+    driver = webdriver.Chrome(r'C:\Users\Mariusz\IdeaProjects\PythonTestExercises3\Additional\Drivers\chromedriver.exe')
     driver.maximize_window()
     # driver.implicitly_wait(30)
     # driver.set_page_load_timeout(30)
